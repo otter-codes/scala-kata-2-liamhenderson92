12db248968fcaa8c8ba6c7036286ee8b1f44ed05
Liam Henderson <liam.henderson@digital.hmrc.gov.uk>
2018-02-08 10:34:30 +0000

    change text

--- src/main/scala/CollatzApp.scala
+++ src/main/scala/CollatzApp.scala
@@ -2,15 +2,15 @@ import scala.io.StdIn
 
 object CollatzApp extends App {
 
-  def collatz(n: Int, c : Int = 0): Int = {
-    if(n == 1) {
-      c+ 1
+  def collatz(num: Int, count : Int = 0): Int = {
+    if(num == 1) {
+      count+ 1
     }
     else {
 
-      if(n % 2 == 0) {collatz(n / 2,c + 1)}
+      if(num % 2 == 0) {collatz(num / 2,count + 1)}
 
-      else {collatz(n*3 + 1, c + 1)}
+      else {collatz(num*3 + 1, count + 1)}
     }
 
 }
